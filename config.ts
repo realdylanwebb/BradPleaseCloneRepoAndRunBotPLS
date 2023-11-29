39e304299ca845d473568fee6d976ed00b91d909
realdylanwebb <dylan.webb8@gmail.com>
2023-11-28 23:14:23 -0700

    Stuff :(

--- config.ts
+++ config.ts
@@ -1,7 +1,6 @@
 const config = {
-  token:
-    "MTEwMjc0OTU0MTk4MTIyOTA1Nw.GxdlNT.HlQ-klA8dN_ryB7gQcNQFTS6Znvo0ZdUM_AKcg",
-  dataApiKey: "AIzaSyC53Napw60XG7V5xhrtqvpeoOs28WQ8WJc",
+  token: "",
+  dataApiKey: "",
 };
 
 export default config;
